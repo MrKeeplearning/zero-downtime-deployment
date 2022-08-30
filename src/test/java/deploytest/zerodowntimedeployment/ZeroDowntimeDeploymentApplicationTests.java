61e148184eab9250a35d787a8729d55b5ef21761
MrKeeplearning <jaeseok1115@gmail.com>
2022-08-30 14:20:30 +0900

    Initial commit

---
+++ src/test/java/deploytest/zerodowntimedeployment/ZeroDowntimeDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package deploytest.zerodowntimedeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroDowntimeDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
